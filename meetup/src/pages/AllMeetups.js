f6e9b5ed8d66b6b3625bf8a94d3e29cce51a3154
JJ <jos.joha@hotmail.com>
2021-09-08 14:59:46 +0200

    new components and prop children
    Added more components and props children to the project

--- meetup/src/pages/AllMeetups.js
+++ meetup/src/pages/AllMeetups.js
@@ -1,5 +1,32 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://lets-travel-more.com/wp-content/uploads/2019/09/dramatic-sunset-in-mediterranean-town-of-oia-PQBBWGQ.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://lets-travel-more.com/wp-content/uploads/2019/09/dramatic-sunset-in-mediterranean-town-of-oia-PQBBWGQ.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <MeetupList meetups={DUMMY_DATA} /> 
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
